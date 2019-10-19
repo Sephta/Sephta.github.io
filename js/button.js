e500bc49194225ad47c2c06f1fbae145829a8516
Seth Tal <Seth.tal255@gmail.com>
2019-10-18 19:18:05 -0700

    Site Switch
    Here is an older real project I worked on back when I first started learning about web development.

---
+++ js/button.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById('menu-nav').style.height = "10vh";
+  document.getElementById('menu-close').style.height = "auto";
+}
+
+function closeNav() {
+  document.getElementById('menu-nav').style.height = "0";
+  document.getElementById('menu-close').style.height = "0";
+}
