52d4cf74ded06187d52b5b346d947ecda2be00ae
Seth Tal <Seth.tal255@gmail.com>
2019-10-20 12:24:57 -0700

    Further edits to the header section
    - added simple animation to close button
    
    - added simple animation to the information button
    
    - moved a couple lines of code around
    
    - in the button.js file, added line of code so that onClick event the info button will dissapear and then re-apear when the close button clicked
    
    - removed the defaulted black background of the header section
    
    - copy/pasted stackoverflow user: Blowsie's code snippet for removing selectable text from the webpage

--- js/button.js
+++ js/button.js
@@ -1,10 +1,11 @@
 function openNav() {
   document.getElementById('menu-nav').style.height = "10vh";
   document.getElementById('menu-close').style.height = "auto";
-  document.getElementById('menu-icon').style.content = "hidden";
+  document.getElementById('menu-icon').style.display = "none";
 }
 
 function closeNav() {
   document.getElementById('menu-nav').style.height = "0";
   document.getElementById('menu-close').style.height = "0";
+  document.getElementById('menu-icon').style.display = "flex";
 }
