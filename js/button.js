71466b0587876d86866ac8d5bbf55d57983368bb
Seth Tal <Seth.tal255@gmail.com>
2019-10-20 21:18:30 -0700

    Code Cleanup and Simple animation integration
    - added quick sliding animation to the information button on landing page
    
    - cleaned up code in header.css and index.html
    
    - other minor changes

--- js/button.js
+++ js/button.js
@@ -1,11 +1,24 @@
+/*
+Seth Tal
+Button.js: a series of functions aimed towards animating various interactive
+           elements of my website...
+*/
+
+
 function openNav() {
+  var menu_icon = document.getElementById('menu-icon');
+
   document.getElementById('menu-nav').style.height = "10vh";
   document.getElementById('menu-close').style.height = "auto";
-  document.getElementById('menu-icon').style.display = "none";
+
+  menu_icon.style.transform = "translate(10000px)"
 }
 
 function closeNav() {
+  var menu_icon = document.getElementById('menu-icon');
+
   document.getElementById('menu-nav').style.height = "0";
   document.getElementById('menu-close').style.height = "0";
-  document.getElementById('menu-icon').style.display = "flex";
+
+  menu_icon.style.transform = "translate(0px)";
 }
