723c8b643c26a8776128b90c404ef15868720dd0
Seth Tal <seth.tal255@gmail.com>
2020-11-29 14:28:54 -0800

    added contact info drop down menu type thing

--- js/main.js
+++ js/main.js
@@ -10,10 +10,18 @@ b1 = document.getElementById("bar-one");
 b2 = document.getElementById("bar-two");
 b3 = document.getElementById("bar-three");
 
+let contactInfo = document.getElementById("contact-info");
+
 function openButton() {
     navContent.classList.toggle('show');
     b1.classList.toggle('x1');
     b2.classList.toggle('x2');
     b3.classList.toggle('x3');
     
+    if (contactInfo.classList.contains("show"))
+        contactInfo.classList.toggle("show");
+}
+
+function openContactInfo() {
+    contactInfo.classList.toggle("show");
 }
