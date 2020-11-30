3938252e7c0bc4ce786bb5b8c8b5b16d9e718a8b
Seth Tal <seth.tal255@gmail.com>
2020-11-30 14:24:31 -0800

    Put some work into making the layout mobile friendly

--- js/main.js
+++ js/main.js
@@ -10,7 +10,11 @@ b1 = document.getElementById("bar-one");
 b2 = document.getElementById("bar-two");
 b3 = document.getElementById("bar-three");
 
-let contactInfo = document.getElementById("contact-info");
+contactInfo = document.getElementById("contact-info");
+ci1 = document.getElementById("ci1");
+ci2 = document.getElementById("ci2");
+ci3 = document.getElementById("ci3");
+ci4 = document.getElementById("ci4");
 
 function openButton() {
     navContent.classList.toggle('show');
@@ -18,10 +22,18 @@ function openButton() {
     b2.classList.toggle('x2');
     b3.classList.toggle('x3');
     
-    if (contactInfo.classList.contains("show"))
-        contactInfo.classList.toggle("show");
+    // if (ci1.classList.contains("show"))
+    //     ci1.classList.toggle("show");
+    // if (ci2.classList.contains("show"))
+    //     ci2.classList.toggle("show");
+    // if (ci3.classList.contains("show"))
+    //     ci3.classList.toggle("show");
+    // if (ci4.classList.contains("show"))
+    //     ci4.classList.toggle("show");
 }
-
 function openContactInfo() {
-    contactInfo.classList.toggle("show");
+    ci1.classList.toggle("show");
+    ci2.classList.toggle("show");
+    ci3.classList.toggle("show");
+    ci4.classList.toggle("show");
 }
